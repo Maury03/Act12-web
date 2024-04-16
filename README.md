21527124757acac416775d406299ba8e57b2c384
Maury03 <diegomqh@gmail.com>
2024-04-16 17:46:09 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Actividad 12
+Página web que utiliza vue para mostrar un arreglo de oraciones usando v-for
---
+++ main.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        chuck: [ 
+        { 
+            value: 'Chuck Norris can skydive into outer space.'
+        }, 
+        { 
+            value: 'The chief export of Chuck Norris is pain.'
+        }, 
+        { 
+            value: 'Chuck Norris doesnt read books. He stares them down until he gets the information he wants.'
+        }, 
+        { 
+            value: 'Time waits for no man. Unless that man is Chuck Norris.'
+        }, 
+        { 
+            value: 'If you spell Chuck Norris in Scrabble, you win. Forever.'
+        }, 
+        ], 
+    }
+})
